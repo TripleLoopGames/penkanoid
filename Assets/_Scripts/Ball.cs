2103257847341b0dc3318effdda20c3f82eb1882
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-11-28 10:01:32 +0100

    feat: add basic bounce ball

---
+++ Assets/_Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+    private Rigidbody2D ownRigidbody;
+    private float lastSpeed = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+        Vector2 randomForce = new Vector2(1, 0.6f);
+        ownRigidbody = GetComponent<Rigidbody2D>();
+        ownRigidbody.AddForce(randomForce * 5, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        float currentSpeed = ownRigidbody.velocity.magnitude;
+        if (currentSpeed != lastSpeed)
+        {
+            Debug.Log(currentSpeed);
+        }
+        lastSpeed = currentSpeed;
+    }
+
+}
---
+++ Assets/_Scripts/BreakoutCool.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakoutCool : MonoBehaviour {
+
+	// Use this for initialization
+
+    private void Initialize()
+    {
+        InitializeBackground()
+        .InitializeScenario();
+    }
+
+    private BreakoutCool InitializeBackground()
+    {
+        // initialization of background
+        return this;
+    }
+
+    private BreakoutCool InitializeScenario()
+    {
+        // initialization of scenario
+        return this;
+    }
+
+    private void Start()
+    {
+        Initialize();
+    }
+}

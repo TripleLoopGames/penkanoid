3adedf7339f95888a622cd0db956bba1eb2c598d
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-12-05 17:19:43 +0100

    feat(MenuGame): add endgame popup

--- Assets/Plugins/StompyRobot/TypeSafe/usr/Resources.Generated.cs
+++ Assets/Plugins/StompyRobot/TypeSafe/usr/Resources.Generated.cs
@@ -745,9 +745,16 @@ public sealed class SRResources {
                 }
             }
             
+            public static global::TypeSafe.PrefabResource EndGame {
+                get {
+                    return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[2]));
+                }
+            }
+            
             private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_resources = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.IResource>(new global::TypeSafe.IResource[] {
                         new global::TypeSafe.PrefabResource("EventSystem", "Game/Ui/EventSystem"),
-                        new global::TypeSafe.PrefabResource("Canvas", "Game/Ui/Canvas")});
+                        new global::TypeSafe.PrefabResource("Canvas", "Game/Ui/Canvas"),
+                        new global::TypeSafe.PrefabResource("EndGame", "Game/Ui/EndGame")});
             
             /// <summary>
             /// Return a list of all resources in this folder.
--- Assets/_Scripts/Game/GameUi.cs
+++ Assets/_Scripts/Game/GameUi.cs
@@ -1,13 +1,44 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Resources = SRResources.Game.Ui;
 
-public class GameUi : MonoBehaviour {
+public class GameUi : MonoBehaviourEx {
 
     public GameUi Initialize()
     {
+        InitializeEndGame();
         return this;
+    }
+
+    private GameUi InitializeEndGame()
+    {
+        GameObject mainPanel = Resources.EndGame.Instantiate();
+        Button[] buttons = mainPanel.GetComponentsInChildren<Button>();
+        buttons = buttons.Select(button => {
+            if(button.name == "Restart")
+            {
+               button.onClick.AddListener(() => OnReStart());
+            }
+            if (button.name == "Menu")
+            {
+               button.onClick.AddListener(() => OnMenu());
+            }
+            return button;
+        }).ToArray();
+        mainPanel.name = "EndGame";
+        mainPanel.transform.SetParent(this.gameObject.transform, false);
+        mainPanel.SetActive(false);
+        return this;
+    }
+
+    private void OnReStart()
+    {
+        Debug.Log("should call a delegate or send a message to re-start the level");
+    }
+
+    private void OnMenu()
+    {
+        Messenger.Publish(new ChangeSceneMessage(SRScenes.Menu));
     }
 
     public GameUi SetCamera(Camera camera)

ca4fd26d8aebacb7a23e6350cc066ab3aee329eb
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-12-28 13:53:19 +0100

    chore: initiate DoTween plugin

--- Assets/Plugins/StompyRobot/TypeSafe/usr/Resources.Generated.cs
+++ Assets/Plugins/StompyRobot/TypeSafe/usr/Resources.Generated.cs
@@ -21,13 +21,20 @@ public sealed class SRResources {
     
     private const string _tsInternal = "1.2.2-Unity5";
     
+    public static global::TypeSafe.Resource<global::DG.Tweening.Core.DOTweenSettings> DOTweenSettings {
+        get {
+            return ((global::TypeSafe.Resource<global::DG.Tweening.Core.DOTweenSettings>)(__ts_internal_resources[0]));
+        }
+    }
+    
     public static global::TypeSafe.PrefabResource EventSystem {
         get {
-            return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[0]));
+            return ((global::TypeSafe.PrefabResource)(__ts_internal_resources[1]));
         }
     }
     
     private static global::System.Collections.Generic.IList<global::TypeSafe.IResource> __ts_internal_resources = new global::System.Collections.ObjectModel.ReadOnlyCollection<global::TypeSafe.IResource>(new global::TypeSafe.IResource[] {
+                new global::TypeSafe.Resource<global::DG.Tweening.Core.DOTweenSettings>("DOTweenSettings", "DOTweenSettings"),
                 new global::TypeSafe.PrefabResource("EventSystem", "EventSystem")});
     
     public sealed class Menu {

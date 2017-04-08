6688e757973f70eb470ba2e42564be1e51898c0a
Marcel Barros Aparici <barros.marcel92@gmail.com>
2017-04-08 19:35:06 +0200

    feat(MenuUi): dirty LevelSelector
    Added close game and open leaderboard buttons

--- Assets/_Scripts/Menu/MenuUi.cs
+++ Assets/_Scripts/Menu/MenuUi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Resources = SRResources.Menu.Ui;
 using UnityEngine;
 using RSG;
+using UnityEngine.UI;
 
 public class MenuUi : MonoBehaviour
 {
@@ -11,14 +12,16 @@ public class MenuUi : MonoBehaviour
     {
         InitializeBackground()
         .InitializeCanvasGroup()
-        .InitializeLevelSelector();
+        .InitializeLevelSelector()
+        .InitializeButtons();
         return this;
-    }  
+    }
 
-    public IPromise<string> WaitForNextLevel(){
+    public IPromise<string> WaitForNextLevel()
+    {
         return this.levelSelector.WaitForNextLevel();
     }
-  
+
     public MenuUi MakeNonInteractable()
     {
         this.canvasGroup.interactable = false;
@@ -51,6 +54,15 @@ public class MenuUi : MonoBehaviour
     public MenuUi InitializeCanvasGroup()
     {
         this.canvasGroup = GetComponent<CanvasGroup>();
+        return this;
+    }
+
+    public MenuUi InitializeButtons()
+    {
+        Button closeGame = Resources.CloseGame.Instantiate().GetComponent<Button>();
+        closeGame.transform.SetParent(this.gameObject.transform, false);
+        Button openLeaderboard = Resources.OpenLeaderboard.Instantiate().GetComponent<Button>(); ;
+        openLeaderboard.transform.SetParent(this.gameObject.transform, false);
         return this;
     }
 

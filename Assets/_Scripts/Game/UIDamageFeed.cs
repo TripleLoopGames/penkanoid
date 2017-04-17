47cd38e35f340c399574d6602a912dd9428801ba
Alberto <alberto.monte.gar@gmail.com>
2017-04-17 14:05:59 +0200

    fix(DamageFeed): Create UI for red flash

---
+++ Assets/_Scripts/Game/UIDamageFeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDamageFeed : MonoBehaviour {
+
+    public UIDamageFeed Initialize()
+    {
+        this.imageFeed = gameObject.GetComponent<Image>();
+        return this;
+    }
+
+    public void AnimationDamageFeed()
+    {
+        imageFeed.DOColor(Color.red, 1.2f)
+        .SetEase(Ease.OutElastic, 0.4f)
+        .SetLoops(1, LoopType.Yoyo);
+    }
+
+    private Image imageFeed;
+}

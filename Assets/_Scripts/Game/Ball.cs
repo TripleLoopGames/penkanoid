1c0e2c24d4caf0ec02ea3caeaecf3bcfa1b86c5f
Marcel Barros Aparici <barros.marcel92@gmail.com>
2017-04-24 20:32:50 +0200

    fix(Ball): remove ball horizontal bounces

--- Assets/_Scripts/Game/Ball.cs
+++ Assets/_Scripts/Game/Ball.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using RSG;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Ball : MonoBehaviourEx
 {
     public override void Awake()
     {
-       base.Awake();
-       this.ownRigidbody = GetComponent<Rigidbody2D>();
-       this.timerComponent = GetComponent<TimerComponent>();
-       
+        base.Awake();
+        this.ownRigidbody = GetComponent<Rigidbody2D>();
+        this.timerComponent = GetComponent<TimerComponent>();
+
     }
 
     public Ball Initialize(Vector2 position, Action despawnBall, GameObject childParticleBall, Action despawnParticleBall)
@@ -20,9 +22,9 @@ public class Ball : MonoBehaviourEx
         this.gameObject.transform.position = position;
         this.despawnOwn = () =>
         {
-            ballParticles.Stop();
-            ballParticles.gameObject.transform.SetParent(null);
-            autodestroyParticlesComponent.SetDeactivateMethod(despawnParticleBall);
+            this.ballParticles.Stop();
+            this.ballParticles.gameObject.transform.SetParent(null);
+            this.autodestroyParticlesComponent.SetDeactivateMethod(despawnParticleBall);
             despawnBall();
         };
 
@@ -53,14 +55,51 @@ public class Ball : MonoBehaviourEx
         {
             SoundData playWallHit = new SoundData(GetInstanceID(), SRResources.Audio.Effects.ReboteProyectil);
             Messenger.Publish(new PlayEffectMessage(playWallHit));
+            DissappearIfNecessary();
             return;
         }
         if (hasCollidedWithLevel)
         {
             SoundData playBlockHit = new SoundData(GetInstanceID(), SRResources.Audio.Effects.tick);
             Messenger.Publish(new PlayEffectMessage(playBlockHit));
+            DissappearIfNecessary();
             return;
         }
+    }
+
+    // We want to avoid horizontal bounces that are too low
+    // because the player has no way to avoid them
+    private Ball DissappearIfNecessary()
+    {
+        if (IsHorizontalBounce())
+        {
+            Debug.Log("boom");
+            this.despawnOwn();
+        }
+        return this;
+    }
+
+    private bool IsHorizontalBounce()
+    {
+        // ignore bounces that happen too high
+        if (this.transform.position.y > -2f)
+        {
+            return false;
+        }
+        Vector2 direction = this.ownRigidbody.velocity;
+        direction.Normalize();
+        // we filter out the balls that go up
+        if(direction.y > 0f)
+        {
+            return false;
+        }  
+        float angle = Vector2.Angle(direction, Vector2.right);
+        // we take only the balls that have an horizontal bounce
+        if(angle < 170 && angle > 10)
+        {
+            return false;
+        }
+        return true;
     }
 
     private Action despawnOwn;
